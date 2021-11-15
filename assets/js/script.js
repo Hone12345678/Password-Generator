a1c653e805545ec4186051082ccf677e83237d8a
CapNemo <46331608+Hone12345678@users.noreply.github.com>
2021-11-14 22:31:47 -0700

    expanded on the user prompts and added more comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,8 @@
-// Assignment code here
 
-
-// Get user character lenth number
-
-
+//function to collect user input to determine password characteristics
 function gatherUserInput(){
+
+  //variables to string value of potential password configuration options
   var lowerCase = "abcdefghijklmnopqrstuvwxyz"
   var upperCase = "abcdefghijklmnopqrstuvwxyz"
   upperCase = upperCase.toUpperCase()
@@ -13,61 +11,73 @@ function gatherUserInput(){
 
   var caseCharCombined = ""
 
-
+  // Get user character lenth number
   var passwordLength = window.prompt(" Please select the disired lenght of your password (must be between 8 and 128 characters)");
+  // if user enters a value that meets the desired lenght the function continues
     if (passwordLength >= 8 && passwordLength <= 128){
-      alert("great")
+      alert("your password wil be " + passwordLength + " characters long")
+   // if user enters a value that does not meet the desired length the function will notify the user and restart the getUserInput function until the conditions are met.
     }else{
-      alert("worng!")
+      alert("whoa whoa whoa, that's too many characters!\nplease enter a length value between 8 and 128.")
       gatherUserInput()
     }
-
-    // if user a value that meets the desired lenth
     
     // prompting the user for their requirments for the password
+
+    // option to allow for Lowercase 
     var userLowerCase = window.confirm("Would you like to allow lowercase characters?");
-    if (userLowerCase) {
+      if (userLowerCase) {
     window.alert("lowercase WILL be included")
     caseCharCombined = caseCharCombined + lowerCase
+      }else{
+    window.alert("lowercase WILL Not ot be included")
     }
 
+    // option to allow for Uppercase
     var userUpperCase = window.confirm("Would you like to allow uppercase characters?");
-    if (userUpperCase ) {
+      if (userUpperCase ) {
     window.alert("uppercase WILL be included")
     caseCharCombined = caseCharCombined + upperCase
+      }else{
+    window.alert("uppercase WILL Not ot be included")
     }
 
+    // option to allow for Specail Characters
     var userSpecChar = window.confirm("Would you like to allow special characters?");
-    if (userSpecChar) {
-      winodw.alert("special characters WILL be included")
+      if (userSpecChar) {
+    winodw.alert("special characters WILL be included")
     caseCharCombined = caseCharCombined + specChar
+      }else{
+    window.alert("special characters WILL Not ot be included")
     }
 
+    // option to allow for Numeric characters
     var userNumChar = window.confirm ("Would you like to allow numeric characters?");
       if (userNumChar) {
-        winodw.alert("numeric characters WILL be included")
-      caseCharCombined = caseCharCombined + numChar
-      
+    winodw.alert("numeric characters WILL be included")
+    caseCharCombined = caseCharCombined + numChar 
+      }else{
+    window.alert("numeric characters WILL Not ot be included")
     }
 
-    
-    generatePassword(caseCharCombined, passwordLength) 
-  
+    generatePassword(caseCharCombined, passwordLength); 
 
   }
 
+
+// End function to collect user input to determine password characteristics
+
+
+// generatePasword takes two variables one with a concatinated string of the password options from the user and the desired password length producing a password made of desired characters and the desired length
 function generatePassword(caseCharCombined, passwordLength) { 
-  gatherUserInput();
   var endPassword = ""
   for (var i = 0; i < passwordLength; i++) {
     var indexNum = Math.floor(Math.random() * caseCharCombined.length)
   endPassword =  endPassword + caseCharCombined[indexNum] 
-  
-  }
   return endPassword;
+  }
 }
-
-// End prompting the user for their requirments for the password
+// End generate password function
 
 
 // Get references to the #generate element
@@ -76,13 +86,16 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password =  generatePassword();
+  var password = gatherUserInput(); 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+//END
